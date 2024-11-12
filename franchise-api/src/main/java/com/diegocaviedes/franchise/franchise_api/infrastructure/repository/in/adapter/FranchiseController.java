bc006ba186a620ece3ddcbe15b8115a67eb9dd62
diego <diego_1996.c.c@gmail.com>
2024-11-11 21:22:04 -0500

    feat: add controller

---
+++ franchise-api/src/main/java/com/diegocaviedes/franchise/franchise_api/infrastructure/repository/in/adapter/FranchiseController.java
@@ -0,0 +1,63 @@
+package com.diegocaviedes.franchise.franchise_api.infrastructure.repository.in.adapter;
+
+import com.diegocaviedes.franchise.franchise_api.application.service.*;
+import com.diegocaviedes.franchise.franchise_api.domain.model.*;
+import com.diegocaviedes.franchise.franchise_api.infrastructure.repository.in.adapter.dto.BranchDTO;
+import com.diegocaviedes.franchise.franchise_api.infrastructure.repository.in.adapter.dto.FranchiseDTO;
+import com.diegocaviedes.franchise.franchise_api.infrastructure.repository.in.adapter.dto.ProductDTO;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/v1/franchise")
+public class FranchiseController {
+
+  private final CreateFranchiseService createFranchiseService;
+  private final UpdateFranchiseService updateFranchiseService;
+  private final AddBranchService addBranchService;
+  private final AddProductService addProductService;
+  private final UpdateStockService updateStockService;
+
+  public FranchiseController(CreateFranchiseService createFranchiseService,
+      UpdateFranchiseService updateFranchiseService, AddBranchService addBranchService,
+      AddProductService addProductService, UpdateStockService updateStockService,) {
+    this.createFranchiseService = createFranchiseService;
+    this.updateFranchiseService = updateFranchiseService;
+    this.addBranchService = addBranchService;
+    this.addProductService = addProductService;
+    this.updateStockService = updateStockService;
+  }
+
+
+  @PostMapping
+  public Mono<FranchiseDTO> createFranchise(@RequestBody FranchiseDTO franchise) {
+    return createFranchiseService.createFranchise(franchise);
+  }
+
+  @PutMapping("/{franchiseId}")
+  public Mono<FranchiseDTO> updateFranchise(@PathVariable String franchiseId,
+      @RequestBody Franchise franchise) {
+    franchise.setId(franchiseId);
+    return updateFranchiseService.updateFranchise(franchise);
+  }
+
+  @PostMapping("/{franchiseId}/branch")
+  public Mono<BranchDTO> addBranch(@PathVariable String franchiseId,
+      @RequestBody BranchDTO branchDTO) {
+    return addBranchService.addBranch(franchiseId, branchDTO);
+  }
+
+  @PostMapping("/{franchiseId}/branch/{branchId}/product")
+  public Mono<ProductDTO> addProduct(@PathVariable String franchiseId,
+      @PathVariable String branchId,
+      @RequestBody ProductDTO productDto) {
+    return addProductService.addProduct(franchiseId, branchId, productDto);
+  }
+
+  @PutMapping("/{franchiseId}/branch/{branchId}/product/{productId}/stock")
+  public void updateStock(@PathVariable String franchiseId, @PathVariable String branchId,
+      @PathVariable String productId, @RequestParam int stock) {
+    updateStockService.updateStock(franchiseId, branchId, productId, stock);
+  }
+
+}
